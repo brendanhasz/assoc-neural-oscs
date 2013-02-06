66689b05b1da4d816175f935e09ac0cd76fcd831
Brendan Hasz <winsto99@gmail.com>
2013-02-06 13:48:49 -0500

    added graphing tools for 1-Single_Oscillator

---
+++ 1-Single_Oscillator/plotter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../graphing
+python -c "import plot_tools; plot_tools.linep('../1-Single_Oscillator/single_PING_group.dat')" &
---
+++ 1-Single_Oscillator/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make
+./single_PING_group
+./plotter.sh
