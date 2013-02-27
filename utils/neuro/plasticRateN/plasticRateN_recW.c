5cc32ea6936dfd1eb4ae4e3ac377389930478624
Brendan Hasz <winsto99@gmail.com>
2013-02-26 19:16:40 -0500

    still working on /utils/neuro/plasticRateN

--- utils/neuro/plasticRateN/plasticRateN_recW.c
+++ utils/neuro/plasticRateN/plasticRateN_recW.c
@@ -41,29 +41,50 @@
 
 void
 plasticRateN_recW(int g, int n, double R[n][g], double R_i[], 
-    double W[n/100][g][g], int W_c[g][g], double W_i[g][g], double tau_w[g][g],
-    double theta_i[g][g], double tau_theta[g][g],
+    double W_t[n/100][g][g], int W_c[g][g], double W[g][g], 
+    double tau_w, double theta[g][g], double tau_theta,
     double gamma[], double tau[], double dt)
 {
 
-	//Variables
-	int t,i,j,l;  //counters
-	double s; //sum of weights
+    //Variables
+    int t,i,j,l;  //counters
+    double s; //sum of weights
+    int uw=10; //update syn weights each uw timesteps
+    int rw=100; //record syn weights each rw timesteps
 
-	//Initial rates
-	for (i=0; i<g; i++){
-		R[0][i] = R_i[i];
-	}
+    //Initial rates
+    for (i=0; i<g; i++){
+	R[0][i] = R_i[i];
+    }
 	
-	//Simulate
-	for (t=1; t<=n; t++){  //for each timestep,
-		l = t-1; //find last timestep
-		for (i=0; i<g; i++){ //update each rate model
-			s = 0;  //Reset sum
-			for (j=0; j<g; j++){ //sum synaptic inputs
-				s += W[j][i]*R[l][j];
-			}
-			R[t][i]=R[l][i]+dt/tau[i]*(-R[l][i]+MAX(0,s+gamma[i])); //integrate
-		}
+    //Simulate
+    for (t=1; t<n; t++){  //for each timestep,
+	l = t-1; //find last timestep
+	for (i=0; i<g; i++){ //update each rate model
+	    s = 0;  //Reset sum
+	    for (j=0; j<g; j++){ //sum synaptic inputs
+	        s += W[j][i]*R[l][j];
+	    }
+	    R[t][i]=R[l][i]+dt/tau[i]*(-R[l][i]+MAX(0,s+gamma[i])); //integrate
 	}
+        if (t%uw==0){ //every once in a while, update weights
+            //REMEMBER, must multiply time constant by 10 then
+            for (i=0; i<g; i++){
+                for (j=0; j<g; j++){
+                    if (W_c[i][j]>0){ //only update if this syn is updatable
+                    W[i][j]=W[i][j]+uw*dt/tau_w*(R[t][j]*R[t][i]*(R[t][j]-theta[i][j]));
+                    theta[i][j]=uw*dt/tau_theta*(R[t][j]*R[t][j]-theta[i][j]);
+                    }
+                }
+            }
+        }
+        if (t%rw==0){ //every once and a while, record weights
+            for (i=0; i<g; i++){
+                for (j=0; j<g; j++){
+                    W_t[t/rw][i][j] = W[i][j];
+                }
+            }
+        }
+    }
+
 }
