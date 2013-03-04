1a6f879642f1cb53b6b4180009a0cdf11f145ac7
Brendan Hasz <winsto99@gmail.com>
2013-03-04 13:45:44 -0500

    started work on 2_3

---
+++ 2-Two_Coupled_Oscillators/2_3-Learned_Synchrony/Learned_Synchrony.c
@@ -0,0 +1,26 @@
+/* Learned_Synchrony.c- shows how 2 oscillators can learn to 
+                        oscillate in-phase
+
+    Copyright 2012
+    Brendan Hasz
+    bhasz@brandeis.edu
+    www.cs.brandeis.edu/~bhasz/main.php
+    Brandeis University, Waltham, MA, USA
+
+*/
+
+#include <stdio.h>
+#include "../../utils/neuro/get_last_period/get_last_period.h"
+#include "../../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW.h"
+#include "../../utils/neuro/pingRateN/pingRateN.h"
+#include "../../utils/fileIO/fileIO.h"
+
+int main(void){
+
+
+
+
+
+    return 0;
+
+}
--- utils/neuro/plasticPingRateN_recW/plotter.sh
+++ utils/neuro/plasticPingRateN_recW/plotter.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd ../../../graphing
 echo "Plotting..."
+
 # Initially in-phase
 python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW_tester_iIN.dat','Two coupled PING oscillators w/ plasticity - started IN-phase','Time (100 us)','Rate (Hz)')" &
 python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW_tester_w_iIN.dat','Two coupled PING oscillators w/ plasticity - started IN-phase','Time (100 us)','Synaptic strength')" &
+
+# Initially out-of-phase
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW_tester_iOUT.dat','Two coupled PING oscillators w/ plasticity - started OUT-of-phase','Time (100 us)','Rate (Hz)')" &
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW_tester_w_iOUT.dat','Two coupled PING oscillators w/ plasticity - started OUT-of-phase','Time (100 us)','Synaptic strength')" &
