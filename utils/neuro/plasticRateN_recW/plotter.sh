c403758c25874cc02f3a1eb1cd67b49415bcd85b
Brendan Hasz <winsto99@gmail.com>
2013-03-03 19:41:16 -0500

    added plasticRateN dir and files, but not yet implemented

--- utils/neuro/plasticRateN_recW/plotter.sh
+++ utils/neuro/plasticRateN_recW/plotter.sh
@@ -2,10 +2,10 @@
 cd ../../../graphing
 echo "Plotting..."
 #initially in-phase
-python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN/plasticRateN_recW_tester_rate.dat','Coupled PING oscillators w/ plasticity - init IN','Time (100 us)','Rate (Hz)')" &
-python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN/plasticRateN_recW_tester_w.dat','Coupled PING oscillators w/ plasticity - init IN','Time','Synaptic strength')" &
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN_recW/plasticRateN_recW_tester_rate.dat','Coupled PING oscillators w/ plasticity - init IN','Time (100 us)','Rate (Hz)')" &
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN_recW/plasticRateN_recW_tester_w.dat','Coupled PING oscillators w/ plasticity - init IN','Time','Synaptic strength')" &
 
 # Initially out-of-phase
-python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN/plasticRateN_recW_tester_rate_iOUT.dat','Coupled PING oscillators w/ plasticity - init OUT','Time (100 us)','Rate (Hz)')" &
-python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN/plasticRateN_recW_tester_w_iOUT.dat','Coupled PING oscillators w/ plasticity - init OUT','Time','Synaptic strength')" &
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN_recW/plasticRateN_recW_tester_rate_iOUT.dat','Coupled PING oscillators w/ plasticity - init OUT','Time (100 us)','Rate (Hz)')" &
+python -c "import plot_tools; plot_tools.linep('../utils/neuro/plasticRateN_recW/plasticRateN_recW_tester_w_iOUT.dat','Coupled PING oscillators w/ plasticity - init OUT','Time','Synaptic strength')" &
 
