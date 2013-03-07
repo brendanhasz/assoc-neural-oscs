45aa806d4266c39a82987cdc725735bd033886c2
Brendan Hasz <winsto99@gmail.com>
2013-03-07 18:57:55 -0500

    working on individualizing plotter routines

--- 2-Two_Coupled_Oscillators/2_3-Learned_Synchrony/plotter.py
+++ 2-Two_Coupled_Oscillators/2_3-Learned_Synchrony/plotter.py
@@ -7,6 +7,8 @@ if path not in sys.path:
     sys.path.insert(1,path)
 import plot_tools
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -24,30 +26,58 @@ plot_tools.linep(fname, tit, xlab, ylab, smooth, xran, yran, xlims, ylims)
 
 # Plasticity, started IN-phase
 # Rate
-fname = 'Learned_Synchrony_plas_IN_r.dat'
-tit = 'Rates, during plasticity run - started IN phase'
-xlab = eval("r'Time (100 $\mu$s)'")
-ylab = 'Rate (Hz)'
-plot_tools.linep(fname, tit, xlab, ylab)
+fnameINr = 'Learned_Synchrony_plas_IN_r.dat'
+titINr = 'Plasticity run - started IN phase'
+#titINr = 'Rates, during plasticity run - started IN phase'
+xlabINr = eval("r'Time (100 $\mu$s)'")
+ylabINr = 'Rate (Hz)'
+#plot_tools.linep(fnameINr, titINr, xlabINr, ylabINr)
 # Weight
-fname = 'Learned_Synchrony_plas_IN_w.dat'
-tit = 'Synaptic Weight over time, during plasticity run - started IN phase'
-xlab = eval("r'Time (100 $\mu$s)'")
-ylab = 'Synaptic weight'
-plot_tools.linep(fname, tit, xlab, ylab)
+fnameINw = 'Learned_Synchrony_plas_IN_w.dat'
+#titINw = 'Synaptic Weight over time, during plasticity run - started IN phase'
+xlabINw = eval("r'Time (ms)'")
+ylabINw = 'Synaptic weight'
+#plot_tools.linep(fnameINw, titINw, xlabINw, ylabINw)
+
+dataINr = np.loadtxt(fnameINr)
+dataINw = np.loadtxt(fnameINw)
+fig = plt.figure()
+ax1 = plt.subplot(2,1,1)
+linesINr = plt.plot(dataINr)
+plt.setp(linesINr, linewidth=2.0)
+plt.title(titINr)
+plt.xlabel(xlabINr)
+plt.ylabel(ylabINr)
+ax2 = plt.subplot(2,1,2)
+linesINw = plt.plot(dataINw)
+plt.setp(linesINw, linewidth=2.0)
+plt.xlabel(xlabINw)
+plt.ylabel(ylabINw)
+plt.show()
+    
+
 
 # After IN plasticity
-fname = 'Learned_Synchrony_postIN_pdvpd.dat'
-tit = 'Steady-state Phase Difference as a function of Initial Phase Difference\n after plasticity, initIN, X-Oxc EI=0.3, IE= -0.5, II=0'
-xlab = eval("r'$\Psi_0 $ (Initial Phase diff)'")
-ylab = eval("r'$\langle \Psi_{SS} \\rangle$ (Avg. Steady-state Phase diff)'")
-xran = [0, 2*math.pi]
-yran = None
-xlims = None
-ylims = [-0.1, math.pi+0.1]
+fnameINa = 'Learned_Synchrony_postIN_pdvpd.dat'
+titINa = 'Steady-state Phase Difference as a function of Initial Phase Difference\n after plasticity, initIN, X-Oxc EI=0.3, IE= -0.5, II=0'
+xlabINa = eval("r'$\Psi_0 $ (Initial Phase diff)'")
+ylabINa = eval("r'$\langle \Psi_{SS} \\rangle$ (Avg. Steady-state Phase diff)'")
+xranINa = [0, 2*math.pi]
+yranINa = None
+xlimsINa = None
+ylimsINa = [-0.1, math.pi+0.1]
 smooth_w = 10
-plot_tools.linep(fname, tit, xlab, ylab, smooth, xran, yran, xlims, ylims)
-
+#plot_tools.linep(fname, tit, xlab, ylab, smooth, xran, yran, xlims, ylims)
+dataINa = np.loadtxt(fnameINa)
+fig = plt.figure()
+linesINa = plt.plot(dataINa)
+plt.setp(linesINa, linewidth=2.0)
+plt.title(titINa)
+plt.xlabel(xlabINa)
+plt.ylabel(ylabINa)
+plt.xlim(xlimsINa)
+plt.ylim(ylimsINa)
+plt.show()
 
 
 # Plasticity, started OUT-of-phase
