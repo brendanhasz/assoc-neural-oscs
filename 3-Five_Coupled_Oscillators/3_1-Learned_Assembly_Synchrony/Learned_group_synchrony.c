cc2645d7aaf21208d698cdccf3272a2056141a4f
Brendan Hasz <winsto99@gmail.com>
2013-03-12 21:59:39 -0400

    got 3_1 working

--- 3-Five_Coupled_Oscillators/3_1-Learned_Assembly_Synchrony/Learned_group_synchrony.c
+++ 3-Five_Coupled_Oscillators/3_1-Learned_Assembly_Synchrony/Learned_group_synchrony.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "../../utils/neuro/get_last_period/get_last_period.h"
+#include "../../utils/basic_math/basic_math.h"
 #include "../../utils/neuro/plasticPingRateN_recW/plasticPingRateN_recW.h"
 #include "../../utils/fileIO/fileIO.h"
 #include "../../utils/multithreads/multithreads.h"
@@ -33,12 +34,13 @@ int main(void){
     int plasticTrials = 10;
     int g=2*no; //number of groups
     double dt = 0.0001;
-    int rw = 100;  //how often to record syn weights
     int t,i,j,k,l;  //counters
-    double W_t[n/rw][g][g]; //Weight matrix over time
     double Re[n][no], R_i[no][2]; //Excitatory rate vector and initial rate vec
+    double R[n][g]; //All groups rate vec
 
     //Init weights
+    int rw = 100;  //how often to record syn weights
+    double W_t[n/rw][g][g]; //Weight matrix over time
     double wW[2][2];    //Within-oscillator synaptic weights
         wW[0][0]=2;         wW[0][1]=2.873; //EE    EI
         wW[1][0]=-2.873;    wW[1][1]=-2;    //IE    II
@@ -62,13 +64,68 @@ int main(void){
             W_1D[g*i+j] = W[i][j];
         }
     }
-    int xEE_c=1, xEI_c=0, xIE_c=0, xII_c=0; //Syn weights allowed to change?
 
+    //Weights alowed to change?
+    int W_c[g][g];
+    for (i=0;i<g;i++){ for (j=0;j<g;j++){
+        if (i%2==0 && j%2==0 && i!=j){ //if we're @ an xEE weight,
+            W_c[i][j]=1;
+        } else {
+            W_c[i][j]=0;
+        }
+    }}
+
+    //Weight bounds?
+    int W_b[g][g];
+    for (i=0;i<g;i++){ for (j=0;j<g;j++){
+        if (i%2==0 && j%2==0 && i!=j){ //if we're @ an xEE weight,
+            W_b[i][j]=0.3;
+        } else {
+            W_b[i][j]=0;
+        }
+    }}
+
+    //Plasticity time constants
+    double t_w = 5000;
+    double t_th = 500;
+
+    double th[g][g];
+    for (i=0;i<g;i++){ for (j=0;j<g;j++){
+        if (i%2==0 && j%2==0 && i!=j){ //if we're @ an xEE weight,
+            th[i][j]=15;
+        } else {
+            th[i][j]=0;
+        }
+    }}
+
+    //Gamma and tau
+    double gamma[g];
+    for (i=0;i<g;i+=2){
+        gamma[i] = 10;      //Excitatory
+        gamma[i+1] = -10;   //Inhibitory
+    }
+    double tau[g];
+    for (i=0;i<g;i+=2){
+        tau[i] = 0.002; //AMPA (Excitatory - 2ms)
+        tau[i+1] = 0.01; //GABA_A (Inhibitory - 10ms)
+    }
 
     //Find initial rate vector
     int p=1000;
     double lp_rates[p][2];
     get_last_period(&p, lp_rates, wW);
+
+    double R_i_A1[no][2];
+            R_i_A1[0][0] = lp_rates[0][0]+gen_rand(); //O1, 2, and 3 are IN-phase,
+            R_i_A1[0][1] = lp_rates[0][1]+gen_rand();
+            R_i_A1[1][0] = lp_rates[0][0]+gen_rand();
+            R_i_A1[1][1] = lp_rates[0][1]+gen_rand();
+            R_i_A1[2][0] = lp_rates[0][0]+gen_rand();
+            R_i_A1[2][1] = lp_rates[0][1]+gen_rand();
+            R_i_A1[3][0] = lp_rates[p/2][0]+gen_rand(); //O4, 5 are OUT-phase
+            R_i_A1[3][1] = lp_rates[p/2][1]+gen_rand();
+            R_i_A1[4][0] = lp_rates[p/2][0]+gen_rand();
+            R_i_A1[4][1] = lp_rates[p/2][1]+gen_rand();
 
 
     //Multithreading stuff
@@ -79,7 +136,6 @@ int main(void){
     int t_divs[NUM_THREADS+1];
     segment_threads(NUM_THREADS, 0, pd_res, t_divs);
 
-    double thesum_o2, thesum_o3, thesum_o4, thesum_o5;
     double o2_vec[pd_res], o3_vec[pd_res], o4_vec[pd_res], o5_vec[pd_res]; 
     int pl_res = 1; //How many plastic runs to do
     double avgphdiffs[pl_res][4];
@@ -116,9 +172,25 @@ int main(void){
     printf("Done with pingRateN 5-group tester - data saved as %s\n", fn_prnt);
  */   
 
-    //TODO: LOOP MULTIPLE TIMES AND CREATE LINE PLOT OF AVG PHDIFFS AFTER REPEATED PLASTICITY RUNS
-    int pr; //plastic loops
-    for (pr=0; pr<pl_res;pr++){
+
+
+/******************************* LOOP THROUGH PLASTIC RUNS *****************/
+
+    //Create new file to store weights
+    char * fn_plas_w = "Learned_group_synchrony_plas_w.dat";
+    FILE * pFile_w = fopen(fn_plas_w,"w"); 
+    //fprintf(pFile_wn, "\n"); fclose(pFile_wn);
+    //FILE * pFile_w = fopen(fn_plas_w,"a"); //now open it for appending
+
+    //Create new file to store weights
+    char * fn_plas_r = "Learned_group_synchrony_plas_r.dat";
+    FILE * pFile_r = fopen(fn_plas_r,"w"); 
+    //fprintf(pFile_rn, "\n"); fclose(pFile_rn);
+    //FILE * pFile_r = fopen(fn_plas_r,"a"); //now open it for appending
+
+
+int pr; //plastic loops
+for (pr=0; pr<pl_res;pr++){
 
     /*********** BEFORE PLASTICITY AVG PHDIFFS *************/
     //Run threads
@@ -154,43 +226,39 @@ int main(void){
 
 
 
-    /*********** DO ALTERNATING A1 / A2 PLASTIC RUNS ******************/
-/*
-    //Create new file to store weights
-    char * fn_plas_w = "Learned_group_synchrony_plas_w.dat";
-    FILE * pFile_wn = fopen(fn_plas_w,"w"); fprintf(pFile_wn, "\n"); fclose(pFile_wn);
-    FILE * pFile_w = fopen(fn_plas_w,"a"); //now open it for appending
-
-    //Create new file to store weights
-    char * fn_plas_r = "Learned_group_synchrony_plas_r.dat";
-    FILE * pFile_rn = fopen(fn_plas_r,"w"); fprintf(pFile_rn, "\n"); fclose(pFile_rn);
-    FILE * pFile_r = fopen(fn_plas_r,"a"); //now open it for appending
+    /*********** DO PLASTIC RUNS WHERE A1 IS IN-PHASE ******************/
+    printf("STARTING PLASTIC RUN %d\n", pr);
 
     //Simulate
-    for (l=0; l<plasticTrials; l++){ //alternate A1, A2
-        //ASSEMBLY 1
-        plasticPingRateN_recW(n,no,Re,R_i_A1,W[0],W[1],W[2],W[3],
-                            xEE_c,xEI_c,xIE_c,xII_c,wW,dt,W_t);    
-        W[0] = (W_t[n/rw-1][0][2]+W_t[n/rw-1][0][2])/2; //set new xEE weight for next run
-        printf("xEE weight after plastic run %d (A1): %f\n",i,W_t[n/rw-1][0][2]);
-        //Append to file for weights
-        for (t=0;t<n/rw;t++){
-            for (i=0;i<g;i++){ for (j=0;j<g;j++){
-                if (i%2==0 && j%2==0 && i!=j){ //if we're @ an xEE weight,
-                    fprintf(pFile_w, "%f\t", W_t[t][i][j]);
-                }
-            }}
-            fprintf(pFile_w, "\n"); //new timestep
-        }
-        //Append to file for rates
-        for (t=0; t<n; t++){ 
-            for (i=0;i<no;i++){
-                fprintf(pFile_r, "%f\t", Re[t][i]);
+    //for (l=0; l<plasticTrials; l++){ //alternate A1, A2
+    //ASSEMBLY 1
+
+    //plasticRateN_recW call
+    plasticRateN_recW(g,n,R,R_i_A1,W_t,W_c,W,W_b,t_w,th,t_th,gamma,tau,dt);
+    //Set 2D matrix returned as W -> W_1D for doing above step
+    for (i=0;i<g;i++){ for (j=0;j<g;j++){
+        W_1D[g*i+j] = W[i][j];
+    }}
+    //Append to file for weights
+    for (t=0;t<n/rw;t++){
+        for (i=0;i<g;i++){ for (j=0;j<g;j++){
+            if (i%2==0 && j%2==0 && i!=j){ //if we're @ an xEE weight,
+                fprintf(pFile_w, "%f\t", W_t[t][i][j]);
             }
-            fprintf(pFile_r, "\n");
+        }}
+        fprintf(pFile_w, "\n"); //new timestep
+    }
+    //Append to file for rates
+    for (t=0; t<n; t++){ 
+        for (i=0;i<no;i++){
+            fprintf(pFile_r, "%f\t", Re[t][i]);
         }
+        fprintf(pFile_r, "\n");
+    }
 
+    printf("DONE WITH PLASTIC RUN %d\n", pr);
 
+/*
         //ASSEMBLY 2
         plasticPingRateN_recW(n,no,Re,R_i_A2,W[0],W[1],W[2],W[3],
                             xEE_c,xEI_c,xIE_c,xII_c,wW,dt,W_t);    
@@ -213,13 +281,14 @@ int main(void){
             fprintf(pFile_r, "\n");
         }
     }
+*/
+
+    }
 
     fclose(pFile_w);
     fclose(pFile_r);
 
-*/
-    }
-
+    printf("Done!\n");
 
     return 0;
 
