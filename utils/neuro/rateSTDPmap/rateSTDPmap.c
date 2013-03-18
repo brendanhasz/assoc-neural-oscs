db7ccd858ae28b3e87881632d4e9699f053e8e3a
Brendan Hasz <winsto99@gmail.com>
2013-03-18 13:46:49 -0400

    added saving of stdp kernel

--- utils/neuro/rateSTDPmap/rateSTDPmap.c
+++ utils/neuro/rateSTDPmap/rateSTDPmap.c
@@ -13,9 +13,10 @@
 */
 
 #include <math.h>
+#include "../../fileIO/fileIO.c"
 
 void
-rateSTDPmap(int n, int g, double dt, double R[n][g], 
+rateSTDP(int n, int g, double dt, double R[n][g], 
             double W[][g][g], double W_0[g][g], int W_c[g][g])
 {
 
@@ -33,15 +34,16 @@ rateSTDPmap(int n, int g, double dt, double R[n][g],
     double tau_n = 34e-3;
     double tau_p = 14e-3;
 
-    //Save STDP kernel
-    //TODO
-
     //Initialize W[0][g][g] to W_0[g][g]
-    //TODO
+    for (ga=0; ga<g; ga++){ 
+        for (gb=0; gb<g; gb++){
+            W[0][ga][gb] = W_0[ga][gb];
+        }
+    }
 
     for (t=wid; t<n-wid; t+=step){ //integrate over all t
-        l = t-1; //last timestep
         i = (t-wid)/step; //this timestep for dw
+        l = (0 < i-1) ? i-1 : 0; //last timestep
         for (ga=0; ga<g; ga++){ for (gb=0; gb<g; gb++){ //for all cnxns
         if ( W[ga][gb] ){ //only alter changeable connections
             sum = 0;
@@ -54,6 +56,17 @@ rateSTDPmap(int n, int g, double dt, double R[n][g],
             W[i][ga][gb] = W[l][ga][gb] + sum*dt/tau_w; //calc weight diff eq
         }}}
     }
+
+    //Save STDP kernel
+    fname = "rateSTDP_STDPkernel.dat";
+    double kernel[2*wid+1];
+    for (tau=0; tau<=wid; tau++){ //int post b pre
+        kernel[tau] = A_n*exp(tau/tau_n);
+    }
+    for (tau=wid; tau<=2*wid; tau++){ //int over tau for pre b post
+        kernel[tau] = A_p*exp(-tau/tau_p);
+    }
+    vsave(2*wid+1, kernel, fname);
 
 }
 
